c78e20b6115fd77ef0b2c052f9220d8973b63aea
tomchccom <dreamkms2014@gmail.com>
2025-05-29 17:27:50 +0900

    git commit -m "feat(security) JWT 토큰 로그인으로 변경 oAuth 인증제거 "

--- src/main/java/com/example/GPT/JWT/JwtUtil.java
+++ src/main/java/com/example/GPT/JWT/JwtUtil.java
@@ -10,10 +10,8 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    /*private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);*/
-    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 20; // 2시간
 
-    // 256비트 길이(32바이트)의 시크릿 키 (예시용, 실제론 환경변수로 관리하세요)
+    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 2; // 2시간
     private static final String SECRET = "0123456789abcdef0123456789abcdef"; // 32자(256비트)
 
     private final Key key;
