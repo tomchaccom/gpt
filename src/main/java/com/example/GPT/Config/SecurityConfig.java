d9d1c2f8058d2e6fa3f8b5ac5e3c8401a6834b4f
tomchccom <dreamkms2014@gmail.com>
2025-05-29 17:28:55 +0900

    git commit -m "feat(security)  oAuth 인증제거 "

--- src/main/java/com/example/GPT/Config/SecurityConfig.java
+++ src/main/java/com/example/GPT/Config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,29 +20,23 @@ public class SecurityConfig {
     }
 
     @Bean
-    public OAuth2SuccessHandler oAuth2SuccessHandler(JwtUtil jwtUtil) {
-        return new OAuth2SuccessHandler(jwtUtil);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http,
-                                           JwtAuthenticationFilter jwtAuthenticationFilter,
-                                           OAuth2SuccessHandler oAuth2SuccessHandler) throws Exception {
+                                           JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> {})  // CORS 설정은 WebMvcConfigurer에서 따로 설정
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // CORS preflight 허용
-                        .requestMatchers(HttpMethod.GET, "/search/**", "/details/**","/supports/**","/chat**","/simple**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/search", "/search/simple").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/login", "/").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login","/sign").permitAll()
                         .requestMatchers("/", "/error", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
-                )
-                .oauth2Login(oauth2 -> oauth2
-                         .successHandler(oAuth2SuccessHandler)
-                        // .defaultSuccessUrl("/oauth2/success",true)
-                        .failureUrl("/loginFailure")
                 )
                 .logout(logout -> logout
                         .logoutSuccessUrl("/")
@@ -51,4 +47,3 @@ public class SecurityConfig {
         return http.build();
     }
 }
-
